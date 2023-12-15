d68be034f2b0fa22e4092d7306e891ca6b657916
Rodrigo Sinastre <rodrigo.sinastre@gmail.com>
2023-12-15 13:26:58 -0300

    testando routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Routes, Router } from 'react-router-dom';
+import React from 'react';
+import Home from './routes/Home';
+import About from './routes/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <ul id="navigation">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/sobre' element={<About />} />
+        </Routes>
+      
     </div>
   );
 }
