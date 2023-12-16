a4cc6f3c3909920e2d7028815eff48261549f833
Rodrigo Sinastre <rodrigo.sinastre@gmail.com>
2023-12-16 17:22:14 -0300

    limpando código

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,26 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Home from './Home';
 
-const divStyle = {
-    color:'white'
-};
-
-class About extends React.Component {
-    
-    render() {
-        return (
-            <div>
-                <h2>About Page</h2>
-                <main>
-                    <p>This section contains information about...</p>
-                    <Link to="/">voltar a home</Link>
-                </main>
-            </div>
-        )
-    }
+const About = () => {
+    return (
+        <div>
+            <h2>About Page</h2>
+            <main>
+                <p>This section contains information about...</p>
+                <Link to="/">back to home</Link>
+            </main>
+        </div>
+    )
 }
-
-
 
 export default About;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,29 +2,16 @@ import '../App.css';
 import React from 'react';
 import logo from '../logo.svg';
 
-class Home extends React.Component {
-    render() {
-        return (
-            <div>
-                <header>
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                </header>
-            </div>
-            
-        );
-    }
+const Home = () => {
 
+    return (
+        <div>
+            <header>
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+        </div>
+
+    );
 }
 
 export default Home;
